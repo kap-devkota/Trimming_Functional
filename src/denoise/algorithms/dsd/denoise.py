dad8acb7c4fdd285bf8c37570ee1b70cca61e754
Henri Schmidt <packerfan73@gmail.com>
2020-04-19 23:15:00 -0400

    Add link prediction code and test on butterfly network.

--- src/denoise/algorithms/dsd/denoise.py
+++ src/denoise/algorithms/dsd/denoise.py
@@ -1,13 +1,22 @@
 from .computations import compute_degree_mat, compute_laplacian, compute_X_normalized
+from denoise.graph.operations import densify
 
-def get_best_scoring_unknown_links(edge_list, embedding = None, no_links = 100, lm = 1):
-    emb_mat = embedding
-    if emb_mat is None:
-        emb_mat  = compute_embedding(edge_list, lm = lm)
+import itertools
+import scipy.spatial.distance as spatial
 
-    existing_edge_dict = {}
-    for ed in edge_list:
-        p, q, wt = ed
-        existing_edge_dict[(p, q)] = wt
+def predict_links(A, lm = 1):
+    """Predicts the most likely links in a graph using the DSD diffusion
+    process.
 
-    return compute_pairwise_distance(emb_mat, no_pairs = no_pairs, existing_edge_dict = existing_edge_dict)
+    Returns a ranked list of (edges, distances) sorted from closest to
+    furthest.
+    """
+    D = compute_degree_mat(A)
+    X = compute_X_normalized(A, D, -1, lm=lm)
+
+    distances = spatial.pdist(X)
+    n = A.shape[0]
+    edges = itertools.combinations(range(n), 2) # generator expression doesn't actualize list :)
+    edges_and_distances = list(zip(edges, distances))
+    edges_and_distances.sort(key=lambda x: x[1])
+    return edges_and_distances
